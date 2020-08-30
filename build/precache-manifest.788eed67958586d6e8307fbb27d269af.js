00b88b1bf728ae0a9a86b1a7c513fc042d75628e
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2020-08-30 18:10:09 +0300

    build

---
+++ build/precache-manifest.788eed67958586d6e8307fbb27d269af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35ad6773ba6269df594fc9df84504a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ef50ed5c69acfd2d37f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b760dc6156344fdb1767",
+    "url": "/static/js/2.7763b8f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7763b8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef50ed5c69acfd2d37f",
+    "url": "/static/js/main.2495da1e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
