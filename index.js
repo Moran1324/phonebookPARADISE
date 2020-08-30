f2aa7a0484f96e27e230a4a09740091e06622a5b
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2020-08-30 17:56:06 +0300

    before heroku

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body :req[content-length]'));
+app.use(express.static('build'));
 
 let persons = [
     {
